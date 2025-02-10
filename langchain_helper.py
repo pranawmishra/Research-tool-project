b9d974bf351c1b28665010bf6c8b4c823379c318
Pranaw Mishra <pranawmishra73@gmail.com>
2025-02-10 22:07:09 +0530

    Updated requirements and imports

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,14 +1,29 @@
 import pickle
 import time
-from langchain import OpenAI
-from langchain.chains import RetrievalQAWithSourcesChain
+from langchain_groq import ChatGroq
+from langchain_cohere import CohereEmbeddings
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredURLLoader
-from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from dotenv import load_dotenv
+import os
 load_dotenv()
-llm = OpenAI(temperature=0.9, max_tokens=500)
+os.environ["COHERE_API_KEY"] = os.getenv("COHERE_API_KEY")
+llm = ChatGroq(model="llama-3.3-70b-versatile", api_key=os.getenv("GROQ_API_KEY"))
+str_output_parser = StrOutputParser()
+qa_prompt = ChatPromptTemplate.from_messages([
+            ("system", """you are helpful assistant who help user in answering questions that is asked to you from provided context. \
+                            Your answer should not be greater than 2 lines. Keep your answer very short and do not include any unnecessary lines.\
+                            You have to use the provided document to answer the question:\
+                            {document}\
+                        """),
+            ("human", "{input}"),
+        ])
 
 def data_loading(urls,mp):
     loader = UnstructuredURLLoader(urls=urls)
@@ -23,20 +38,21 @@ def splitting_data(data,mp):
     docs = text_splitter.split_documents(data)
     return docs
 
-def creating_embedding(docs,mp):
-    embedding = OpenAIEmbeddings()
-    vectorstore_openai = FAISS.from_documents(docs,embedding)
+def creating_embedding(docs,embedding,mp):
+    vectorstore = FAISS.from_documents(docs,embedding)
     mp.text('Embedding vector started building.....Please wait......')
     time.sleep(2)
-    return vectorstore_openai
+    return vectorstore
 
-def provide_answer_and_source(file_path,query,st):
-    with open(file_path, "rb") as f:
-        vectorstore = pickle.load(f)
-        chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
-        result = chain({"question": query}, return_only_outputs=True)
-        # result will be a dictionary of this format --> {"answer": "", "sources": [] }
-        return result["answer"], result
+def provide_answer_and_source(vectorstore,query,st):
+    # with open(file_path, "rb") as f:
+    # vectorstore = pickle.load(f)
+    retriever = vectorstore.as_retriever()
+    rag_chain = qa_prompt | llm | str_output_parser
+    result = rag_chain.invoke({"input":query,
+                                "document":retriever.invoke(query)})
+    # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+    return result
 
         # Display sources, if available
 def get_source(result,st):
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import streamlit as st
 import pickle
+from langchain_cohere import CohereEmbeddings
+from langchain_community.vectorstores import FAISS
 # import time
 # from langchain import OpenAI
 # from langchain.chains import RetrievalQAWithSourcesChain
@@ -10,9 +12,10 @@ import pickle
 # from langchain.vectorstores import FAISS
 import langchain_helper
 
-
-st.title('News Research tool')
-st.sidebar.title('News article URLs')
+os.environ["COHERE_API_KEY"] = os.getenv("COHERE_API_KEY")
+embedding = CohereEmbeddings(model="embed-english-light-v3.0")
+st.title('General Research tool')
+st.sidebar.title(' URLs')
 
 urls = []
 
@@ -21,7 +24,6 @@ for i in range(3):
     urls.append(url)
 
 process_url_clicked = st.sidebar.button('Process URL')
-file_path = 'faiss_store_openai.pkl'
 
 main_placeholder = st.empty()
 if process_url_clicked:
@@ -30,22 +32,18 @@ if process_url_clicked:
     # split data
     docs = langchain_helper.splitting_data(data,main_placeholder)
     # create embeddings and save it to FAISS index
-    vectorstore_openai = langchain_helper.creating_embedding(docs,main_placeholder)
-
+    vectorstore = langchain_helper.creating_embedding(docs,embedding,main_placeholder)
     # Save the FAISS index to a pickle file
-    with open(file_path, "wb") as f:
-        pickle.dump(vectorstore_openai, f)
+    vectorstore.save_local("faiss_index")
+
+
 
 query = main_placeholder.text_input('Question: ')
 if query:
-    if os.path.exists(file_path):
-        answer,result = langchain_helper.provide_answer_and_source(file_path, query, st)
-        st.header("Answer")
-        st.write(answer)
-
-        # display source if available
-        sources_list = langchain_helper.get_source(result,st)
-        for source in sources_list:
-            st.write(source)
+    st.text("Loading vectorstore......Please Wait......")
+    vectorstore = FAISS.load_local("faiss_index", embedding, allow_dangerous_deserialization=True)
+    answer = langchain_helper.provide_answer_and_source(vectorstore, query, st)
+    st.header("Answer")
+    st.write(answer)
 
 
